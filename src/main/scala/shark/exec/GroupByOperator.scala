32cb821f82be4528a2cb29bd59e2761c7da2a95e
Cliff Engle <cliffengle@gmail.com>
2012-05-16 12:39:41 -0700

    Removed old comments

--- src/main/scala/shark/exec/GroupByOperator.scala
+++ src/main/scala/shark/exec/GroupByOperator.scala
@@ -13,9 +13,6 @@ import org.apache.hadoop.hive.ql.plan.GroupByDesc
 object GroupByOperator {
   
   def isPostShuffle(op: HiveGroupByOperator): Boolean = {
-    /*return (op.getConf.getMode == GroupByDesc.Mode.FINAL ||
-            op.getConf.getMode == GroupByDesc.Mode.MERGEPARTIAL ||
-            op.getConf.getMode == GroupByDesc.Mode.COMPLETE)*/
     op.getParentOperators().get(0).isInstanceOf[HiveReduceSinkOperator]
   }
   
