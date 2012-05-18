de2496ca22fac263d421031d2a6280d060edeb25
Cliff Engle <cliffengle@gmail.com>
2012-05-18 13:57:32 -0700

    Added custom byte array serializer to Kryo. Fixes #22

--- src/main/scala/shark/KryoRegistrator.scala
+++ src/main/scala/shark/KryoRegistrator.scala
@@ -3,6 +3,8 @@ package shark
 import com.esotericsoftware.kryo.Kryo
 import de.javakaffee.kryoserializers.ArraysAsListSerializer
 import java.util.Arrays
+import java.nio.ByteBuffer
+import com.esotericsoftware.kryo.serialize.{IntSerializer, SimpleSerializer}
 
 class KryoRegistrator extends spark.KryoRegistrator {
   def registerClasses(kryo: Kryo) {
@@ -12,5 +14,21 @@ class KryoRegistrator extends spark.KryoRegistrator {
     // Java Arrays.asList returns an internal class that cannot be serialized
     // by default Kryo. This provides a workaround.
     kryo.register(Arrays.asList().getClass, new ArraysAsListSerializer(kryo))
+    
+    // This is a work-around for Kryo's byte-by-byte serialization of byte
+    // arrays. Note that it does not handle nulls because null rows are already
+    // encoded using Hive's SerDe.
+    kryo.register(classOf[Array[Byte]], new SimpleSerializer[Array[Byte]]() {
+      def write (buffer: ByteBuffer, arr: Array[Byte]) {
+        IntSerializer.put(buffer, arr.length, true)
+        buffer.put(arr)
+      }
+      def read (buffer: ByteBuffer): Array[Byte] =  {
+        val len = IntSerializer.get(buffer, true)
+        val arr = new Array[Byte](len)
+        buffer.get(arr)
+        arr
+      }
+    })
   }
 }
