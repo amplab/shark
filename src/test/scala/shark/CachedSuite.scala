ea229a23d4fbbf13bf9fa6645a40b257bcd32700
Denny <dennybritz@gmail.com>
2012-10-10 13:28:00 -0700

    Text caching of complex types

--- src/test/scala/shark/CachedSuite.scala
+++ src/test/scala/shark/CachedSuite.scala
@@ -2,15 +2,25 @@ package shark
 
 class CachedSuite extends SharkShellCliSuite {
 
-  test("Simple Cached Table") {
+  test("Cached table with simple types") {
     val dataFilePath = System.getenv("HIVE_DEV_HOME") + "/data/files/kv1.txt"
     executeQuery("drop table if exists test;");
-    executeQuery("drop table if exists test_cached;");
     executeQuery("create table test(key int, val string);")
     executeQuery("load data local inpath '" + dataFilePath+ "' overwrite into table test;")
-    executeQuery("create table test_cached as select * from test; ")
+    executeQuery("create table test_cached as select * from test;")
     val out = executeQuery("select * from test_cached where key = 407;")
     assert(out.contains("val_407"))
+  }
+
+  test("Cached Table with complex types") {
+    val dataFilePath = System.getenv("HIVE_DEV_HOME") + "/data/files/create_nested_type.txt"
+    executeQuery("drop table if exists table1;");
+    executeQuery("drop table if exists table1_cached;");
+    executeQuery("CREATE TABLE table1 (a STRING, b ARRAY<STRING>, c ARRAY<MAP<STRING,STRING>>, d MAP<STRING,ARRAY<STRING>>);")
+    executeQuery("load data local inpath '" + dataFilePath+ "' overwrite into table table1;")
+    executeQuery("""create table table1_cached TBLPROPERTIES ("shark.cache" = "true") as select * from table1;""")
+    val out = executeQuery("select * from table1_cached where a = 'a0';")
+    assert(out.contains("""{"c001":"C001","c002":"C002"}"""))
   }
 
 }
--- src/test/scala/shark/SharkServerSuite.scala
+++ src/test/scala/shark/SharkServerSuite.scala
@@ -45,11 +45,9 @@ class SharkServerSuite extends SharkCliSuite {
   test("Simple Query against Shark Server") {
     val dataFilePath = System.getenv("HIVE_DEV_HOME") + "/data/files/kv1.txt"
     executeQuery("drop table if exists test;");
-    executeQuery("drop table if exists test_cached;");
     executeQuery("create table test(key int, val string);")
     executeQuery("load data local inpath '" + dataFilePath+ "' overwrite into table test;")
-    executeQuery("create table test_cached as select * from test;")
-    val out = executeQuery("select * from test_cached where key = 407;")
+    val out = executeQuery("select * from test where key = 407;")
     assert(out.contains("val_407"))
   }
 
