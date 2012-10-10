7c3aacf2e77f58e9cff64d9775066bab99a3ba90
Denny <dennybritz@gmail.com>
2012-10-10 08:43:13 -0700

    Merged ServerSuites

--- src/test/scala/shark/SharkServerSuite.scala
+++ src/test/scala/shark/SharkServerSuite.scala
@@ -3,7 +3,7 @@ package shark
 import java.io.{BufferedReader, InputStreamReader, PrintWriter}
 import org.scalatest.{BeforeAndAfter, FunSuite}
 
-abstract class SharkServerSuite extends SharkCliSuite {
+class SharkServerSuite extends SharkCliSuite {
 
   var serverProcess : Process = null
   var serverInputReader : BufferedReader = null
@@ -22,7 +22,7 @@ abstract class SharkServerSuite extends SharkCliSuite {
     outputWriter = new PrintWriter(process.getOutputStream, true)
     inputReader = new BufferedReader(new InputStreamReader(process.getInputStream))
     errorReader = new BufferedReader(new InputStreamReader(process.getErrorStream))
-    waitForOutput(inputReader, ">", 25000)
+    waitForOutput(inputReader, "shark>", 25000)
   }
 
   after {
@@ -40,6 +40,17 @@ abstract class SharkServerSuite extends SharkCliSuite {
       assert(false)
       return null
     }
+  }
+
+  test("Simple Query against Shark Server") {
+    val dataFilePath = System.getenv("HIVE_DEV_HOME") + "/data/files/kv1.txt"
+    executeQuery("drop table if exists test;");
+    executeQuery("drop table if exists test_cached;");
+    executeQuery("create table test(key int, val string);")
+    executeQuery("load data local inpath '" + dataFilePath+ "' overwrite into table test;")
+    executeQuery("create table test_cached as select * from test;")
+    val out = executeQuery("select * from test_cached where key = 407;")
+    assert(out.contains("val_407"))
   }
 
 }
