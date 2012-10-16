04ecfc2a68b1fc31c964b8c1dc55a0caf2d719a9
Reynold Xin <rxin@cs.berkeley.edu>
2012-10-15 23:03:25 -0700

    Fixed commit path from the previous cherry pick.

--- src/main/scala/shark/SharkEnv.scala
+++ src/main/scala/shark/SharkEnv.scala
@@ -5,7 +5,6 @@ import scala.collection.mutable.{HashMap, HashSet}
 import org.apache.hadoop.hive.ql.metadata.Hive
 import org.apache.hadoop.hive.conf.HiveConf
 
-import shark.memstore.CacheManager
 import spark.SparkContext
 
 /** A singleton object for the master program. The slaves should not access this. */
