1a360a7f1fb0946bdceb719be627f74ad3853842
Reynold Xin <rxin@cs.berkeley.edu>
2012-11-21 17:30:05 -0800

    Updated Spark version to 0.6.1

--- project/SharkBuild.scala
+++ project/SharkBuild.scala
@@ -10,7 +10,7 @@ object SharkBuild extends Build {
   val HADOOP_VERSION = "0.20.205.0"
 
   // Spark version to build against.
-  val SPARK_VERSION = "0.6.0"
+  val SPARK_VERSION = "0.6.1"
 
   lazy val root = Project(
     id = "root",
