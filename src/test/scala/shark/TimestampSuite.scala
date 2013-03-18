4d63d7325c5131a1ce9c338ce88634fd3d072660
Mikhail Bautin <mbautin@gmail.com>
2013-03-18 01:20:08 -0700

    Reproduce the bug with GROUP BY and ORDER BY for timestamps

---
+++ src/test/scala/shark/TimestampSuite.scala
@@ -0,0 +1,42 @@
+package shark
+
+import org.junit.Assert._
+import java.io.{FileOutputStream, PrintWriter, BufferedOutputStream, File}
+import org.junit.{Before, Test}
+
+/**
+ * Test various queries related to timestamps. Specifically, test for a bug that is occurs when
+ * counting timestamp values, ordering by the same timestamp column.
+ */
+class TimestampSuite extends SharkHiveTestUtil {
+
+  @Before
+  def setUp() {
+    setHiveTestDir()
+  }
+
+  @Test
+  def testCountGroupByOrderByTimestamp() {
+    SharkEnv.initWithSharkContext("CountGroupByOrderByTimestamp")
+    val tableName = "ts_test"
+    val testDataFile = new File(getTestDir, tableName + ".tsv")
+    val out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(testDataFile)))
+    val tsStr = "2013-03-18 00:41:15"
+    out.println(tsStr)
+    out.close()
+
+    try {
+      val sc = SharkEnv.sc.asInstanceOf[SharkContext]
+      assertEquals("", sc.sql("DROP TABLE IF EXISTS %s".format(tableName)).mkString("\n"))
+      assertEquals("", sc.sql("CREATE TABLE %s (t TIMESTAMP)".format(tableName)).mkString("\n"))
+      assertEquals("", sc.sql("LOAD DATA LOCAL INPATH '%s' OVERWRITE INTO TABLE %s".format(
+        testDataFile.getAbsolutePath, tableName)).mkString("\n"))
+      assertEquals(tsStr, sc.sql("SELECT * FROM %s".format(tableName)).mkString("\n"))
+      assertEquals(tsStr + "\t1",
+        sc.sql("SELECT t, COUNT(1) FROM %s GROUP BY t ORDER BY t".format(tableName)).mkString("\n"))
+    } finally {
+      SharkEnv.stop()
+    }
+  }
+
+}
