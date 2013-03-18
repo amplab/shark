25e85d2b4894a4f397d676b9efeb44a22ce6bed5
Mikhail Bautin <mbautin@gmail.com>
2013-03-18 10:38:40 -0700

    Make TimestampSuite

--- src/test/scala/shark/TimestampSuite.scala
+++ src/test/scala/shark/TimestampSuite.scala
@@ -1,24 +1,23 @@
 package shark
 
-import org.junit.Assert._
 import java.io.{FileOutputStream, PrintWriter, BufferedOutputStream, File}
-import org.junit.{Before, Test}
+import org.scalatest.{FunSuite, BeforeAndAfterAll}
+import org.junit.Assert._
 
 /**
  * Test various queries related to timestamps. Specifically, test for a bug that is occurs when
  * counting timestamp values, ordering by the same timestamp column.
  */
-class TimestampSuite extends SharkHiveTestUtil {
+class TimestampSuite extends FunSuite with SharkHiveTestUtil with BeforeAndAfterAll {
 
-  @Before
-  def setUp() {
+  override def beforeAll() {
     setHiveTestDir()
   }
 
-  @Test
-  def testCountGroupByOrderByTimestamp() {
+  test("CountGroupByOrderByTimestamp") {
     SharkEnv.initWithSharkContext("CountGroupByOrderByTimestamp")
     val tableName = "ts_test"
+    new File(getTestDir).mkdirs()
     val testDataFile = new File(getTestDir, tableName + ".tsv")
     val out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(testDataFile)))
     val tsStr = "2013-03-18 00:41:15"
