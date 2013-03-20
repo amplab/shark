9783553918f91b1589556ac00eb45ee08c6d6bf7
Nandu Jayakumar <nandu@yahoo-inc.com>
2013-03-19 17:43:05 -0700

    Version 0.2.0.1         First cut 	All external deps are brought in as jars 	Standard yinst directory structure not in use. 	Symlink /home/y/libexec/miners_shark/ will always be available 	and kept current pointing to 	/home/y/libexec/miners_shark-VERSION 	Added a conf file & yinst setting to set SCALA_HOME, 	SPARK_HOME etc... 	yinst set params control where deps are picked up from - see 	yicf.

---
+++ yinst/shark-env.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+# Copyright (C) 2012 The Regents of The University California.
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# (Required) Amount of memory used per slave node. This should be in the same
+# format as the JVM's -Xmx option, e.g. 300m or 1g.
+export SPARK_MEM=4g
+
+# (Required) Set the master program's memory
+export SHARK_MASTER_MEM=4g
+
+# (Required) Point to your Scala installation.
+export SCALA_HOME=$(miners_shark__SCALA_HOME)
+
+# (Required) Point to the patched Hive binary distribution
+export HIVE_HOME=$(miners_shark__HIVE_HOME)
+
+# (Optional) Specify the location of Hive's configuration directory. By default,
+# it points to $HIVE_HOME/conf
+#export HIVE_CONF_DIR="$HIVE_HOME/conf"
+
+# For running Shark in distributed mode, set the following:
+export HADOOP_HOME=$(miners_shark__HADOOP_HOME)
+export HADOOP_PREFIX=$(miners_shark__HADOOP_PREFIX)
+export SPARK_HOME=$(miners_shark__SPARK_HOME)
+#export MASTER=""
+#export MESOS_NATIVE_LIBRARY=/usr/local/lib/libmesos.so
+
+# (Optional) Extra classpath
+#export SPARK_LIBRARY_PATH=""
+
+# Java options
+# On EC2, change the local.dir to /mnt/tmp
+SPARK_JAVA_OPTS="-Dspark.local.dir=/tmp "
+SPARK_JAVA_OPTS+="-Dspark.kryoserializer.buffer.mb=10 "
+SPARK_JAVA_OPTS+="-verbose:gc -XX:-PrintGCDetails -XX:+PrintGCTimeStamps "
+export SPARK_JAVA_OPTS
