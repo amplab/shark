4c80a6c582ebf3330e0ec6de223ce28d8ad5dcbd
Mark Hamstra <markhamstra@gmail.com>
2013-04-09 14:42:40 -0700

    Syncing dependency versions

--- project/SharkBuild.scala
+++ project/SharkBuild.scala
@@ -91,7 +91,7 @@ object SharkBuild extends Build {
       "org.spark-project" %% "spark-repl" % SPARK_VERSION,
       "com.google.guava" % "guava" % "11.0.1",
       "org.apache.hadoop" % "hadoop-core" % HADOOP_VERSION,
-      "it.unimi.dsi" % "fastutil" % "6.4.2",
+      "it.unimi.dsi" % "fastutil" % "6.4.4",
       "org.scalatest" %% "scalatest" % "1.9.1" % "test",
       "junit" % "junit" % "4.10" % "test",
       "com.novocode" % "junit-interface" % "0.8" % "test") ++
