ea5145e87e14545c6ff35799b4d9333dfec84b2c
Reynold Xin <rxin@cs.berkeley.edu>
2013-05-10 00:52:23 -0700

    Improved testing using SharkContext.sql2rdd and SharkContext.sql.

--- src/test/scala/shark/SQLSuite.scala
+++ src/test/scala/shark/SQLSuite.scala
@@ -20,12 +20,13 @@ package shark
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.FunSuite
 
+import shark.api.QueryExecutionException
+
 
 class SQLSuite extends FunSuite with BeforeAndAfterAll {
 
   val WAREHOUSE_PATH = CliTestToolkit.getWarehousePath("sql")
   val METASTORE_PATH = CliTestToolkit.getMetastorePath("sql")
-
   val MASTER = "local"
 
   var sc: SharkContext = _
@@ -35,31 +36,200 @@ class SQLSuite extends FunSuite with BeforeAndAfterAll {
     sc.sql("set javax.jdo.option.ConnectionURL=jdbc:derby:;databaseName=" +
       METASTORE_PATH + ";create=true")
     sc.sql("set hive.metastore.warehouse.dir=" + WAREHOUSE_PATH)
-    sc.sql("CREATE TABLE src(key INT, value STRING)")
-    sc.sql("LOAD DATA LOCAL INPATH '${env:HIVE_HOME}/examples/files/kv1.txt' INTO TABLE src")
-    sc.sql("CREATE TABLE src_cached AS SELECT * FROM SRC")
+    sc.sql("set shark.test.data.path=" + CliTestToolkit.dataFilePath)
+
+    // test
+    sc.sql("CREATE TABLE test (key INT, val STRING)")
+    sc.sql("LOAD DATA LOCAL INPATH '${hiveconf:shark.test.data.path}/kv1.txt' INTO TABLE test")
+    sc.sql("CREATE TABLE test_cached AS SELECT * FROM test")
+
+    // clicks
+    sc.sql("""create table clicks (id int, click int)
+      row format delimited fields terminated by '\t'""")
+    sc.sql("""load data local inpath '${hiveconf:shark.test.data.path}/clicks.txt'
+      OVERWRITE INTO TABLE clicks""")
+    sc.sql("create table clicks_cached as select * from clicks")
+
+    // users
+    sc.sql("""create table users (id int, name string)
+      row format delimited fields terminated by '\t'""")
+    sc.sql("""load data local inpath '${hiveconf:shark.test.data.path}/users.txt'
+      OVERWRITE INTO TABLE users""")
+    sc.sql("create table users_cached as select * from users")
   }
 
   override def afterAll() {
     sc.stop()
   }
 
+  private def expect(sql: String, expectedResults: Array[String]) {
+    val results = sc.sql(sql).sortWith(_ < _)
+    val expected = expectedResults.sortWith(_ < _)
+    assert(results.corresponds(expected)(_.equals(_)),
+      "Expected: " + expected.mkString("\n") + "; got " + results.mkString("\n"))
+  }
+
+  // A shortcut for single row results.
+  private def expect(sql: String, expectedResult: String) {
+    expect(sql, Array(expectedResult))
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+  // basic SQL
+  //////////////////////////////////////////////////////////////////////////////
   test("count") {
-    expect("select count(*) from src", Array("500"))
-    expect("select count(*) from src_cached", Array("500"))
+    expect("select count(*) from test", "500")
+    expect("select count(*) from test_cached", "500")
   }
 
   test("filter") {
-    expect("select * from src where key=100 or key=497",
+    expect("select * from test where key=100 or key=497",
       Array("100\tval_100", "100\tval_100", "497\tval_497"))
-    expect("select * from src_cached where key=100 or key=497",
+    expect("select * from test_cached where key=100 or key=497",
       Array("100\tval_100", "100\tval_100", "497\tval_497"))
   }
 
-  def expect(sql: String, expectedResults: Array[String]) {
-    val results = sc.sql(sql).sortWith(_ < _)
-    val expected = expectedResults.sortWith(_ < _)
-    assert(results.corresponds(expected)(_.equals(_)), "Expected: " + expected.mkString + "; got " + results.mkString)
+  test("count distinct") {
+    expect("select count(distinct key) from test", "309")
+    expect("select count(distinct key) from test_cached", "309")
   }
 
+  test("count bigint") {
+    sc.sql("create table test_bigint (key bigint, val string)")
+    sc.sql("""load data local inpath '${hiveconf:shark.test.data.path}/kv1.txt'
+      OVERWRITE INTO TABLE test_bigint""")
+    sc.sql("create table test_bigint_cached as select * from test_bigint")
+    expect("select val, count(*) from test_bigint_cached where key=484 group by val", "val_484\t1")
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+  // column pruning
+  //////////////////////////////////////////////////////////////////////////////
+  test("column pruning filters") {
+    expect("select count(*) from test_cached where key > -1", "500")
+  }
+
+  test("column pruning group by") {
+    expect("select key, count(*) from test_cached group by key order by key limit 1", "0\t3")
+  }
+
+  test("column pruning group by with single filter") {
+    expect("select key, count(*) from test_cached where val='val_484' group by key", "484\t1")
+  }
+
+  test("column pruning aggregate function") {
+    expect("select val, sum(key) from test_cached group by val order by val desc limit 1",
+      "val_98\t196")
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+  // map join
+  //////////////////////////////////////////////////////////////////////////////
+  test("map join") {
+    expect("""select u.name, count(c.click) from clicks c join users u on (c.id = u.id)
+      group by u.name having u.name='A'""",
+      "A\t3")
+  }
+
+  test("map join2") {
+    expect("select count(*) from clicks join users on (clicks.id = users.id)", "5")
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+  // cache DDL
+  //////////////////////////////////////////////////////////////////////////////
+  test("insert into cached tables") {
+    sc.sql("create table test1_cached as select * from test")
+    expect("select count(*) from test1_cached", "500")
+    sc.sql("insert into table test1_cached select * from test where key > -1 limit 499")
+    expect("select count(*) from test1_cached", "999")
+  }
+
+  test("insert overwrite") {
+    sc.sql("create table test2_cached as select * from test")
+    expect("select count(*) from test2_cached", "500")
+    sc.sql("insert overwrite table test2_cached select * from test where key > -1 limit 499")
+    expect("select count(*) from test2_cached", "499")
+  }
+
+  test("error when attempting to update cached table(s) using command with multiple INSERTs") {
+    sc.sql("create table multi_insert_test as select * from test")
+    sc.sql("create table multi_insert_test_cached as select * from test")
+    intercept[QueryExecutionException] {
+      sc.sql("""from test
+        insert into table multi_insert_test select *
+        insert into table multi_insert_test_cached select *""")
+    }
+  }
+
+  test("drop partition") {
+    sc.sql("create table foo_cached(key int, val string) partitioned by (dt string)")
+    sc.sql("insert overwrite table foo_cached partition(dt='100') select * from test")
+    expect("select count(*) from foo_cached", "500")
+    sc.sql("alter table foo_cached drop partition(dt='100')")
+    expect("select count(*) from foo_cached", "0")
+  }
+
+  test("create cached table with table properties") {
+    sc.sql("""create table ctas_tbl_props TBLPROPERTIES ('shark.cache'='true') as
+      select * from test""")
+    assert(SharkEnv.memoryMetadataManager.contains("ctas_tbl_props"))
+    expect("select * from ctas_tbl_props where key=407", "407\tval_407")
+  }
+
+  test("cached tables with complex types") {
+    sc.sql("""CREATE TABLE test_complex_types (
+      a STRING, b ARRAY<STRING>, c ARRAY<MAP<STRING,STRING>>, d MAP<STRING,ARRAY<STRING>>)""")
+    sc.sql("""load data local inpath '${hiveconf:shark.test.data.path}/create_nested_type.txt'
+      overwrite into table test_complex_types""")
+    sc.sql("""create table test_complex_types_cached TBLPROPERTIES ("shark.cache" = "true") as
+      select * from test_complex_types""")
+    expect("select a from test_complex_types_cached where a = 'a0'", """a0""")
+    expect("select b from test_complex_types_cached where a = 'a0'", """["b00","b01"]""")
+    expect("select c from test_complex_types_cached where a = 'a0'",
+      """[{"c001":"C001","c002":"C002"},{"c011":null,"c012":"C012"}]""")
+    expect("select d from test_complex_types_cached where a = 'a0'",
+      """{"d01":["d011","d012"],"d02":["d021","d022"]}""")
+    assert(SharkEnv.memoryMetadataManager.contains("test_complex_types_cached"))
+  }
+
+  test("disable caching by default") {
+    sc.sql("set shark.cache.flag.checkTableName=false")
+    sc.sql("create table should_not_be_cached as select * from test")
+    expect("select key from should_not_be_cached where key = 407", "407")
+    assert(!SharkEnv.memoryMetadataManager.contains("should_not_be_cached"))
+    sc.sql("set shark.cache.flag.checkTableName=true")
+  }
+
+  test("cached table name should be case-insensitive") {
+    sc.sql("""create table sharkTest5Cached TBLPROPERTIES ("shark.cache" = "true") as
+      select * from test""")
+    expect("select val from sharktest5Cached where key = 407", "val_407")
+    assert(SharkEnv.memoryMetadataManager.contains("sharkTest5Cached"))
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+  // SharkContext APIs (e.g. sql2rdd, sql)
+  //////////////////////////////////////////////////////////////////////////////
+
+  test("sql2rdd") {
+    var rdd = sc.sql2rdd("select * from test")
+    assert(rdd.count === 500)
+    rdd = sc.sql2rdd("select * from test_cached")
+    assert(rdd.count === 500)
+    val collected = rdd.map(r => r.getInt(0)).collect().sortWith(_ < _)
+    assert(collected(0) === 0)
+    assert(collected(499) === 498)
+    assert(collected.size === 500)
+  }
+
+  test("sql exception") {
+    val e = intercept[QueryExecutionException] { sc.sql("asdfasdfasdfasdf") }
+    e.getMessage.contains("semantic")
+  }
+
+  test("sql2rdd exception") {
+    val e = intercept[QueryExecutionException] { sc.sql2rdd("asdfasdfasdfasdf") }
+    e.getMessage.contains("semantic")
+  }
 }
--- src/test/scala/shark/SharkServerSuite.scala
+++ src/test/scala/shark/SharkServerSuite.scala
@@ -1,19 +1,19 @@
 package shark
 
-import java.io.{BufferedReader, InputStreamReader, PrintWriter}
-import org.scalatest.{BeforeAndAfterAll, FunSuite, BeforeAndAfterEach, FlatSpec}
+import java.io.{BufferedReader, InputStreamReader}
 import java.sql.DriverManager
-import org.scalatest.Assertions
 import java.sql.Statement
-import java.util.concurrent.CountDownLatch
 import java.sql.Connection
-import scala.actors.Actor
-import java.util.concurrent.ConcurrentHashMap
-import org.scalatest.matchers.ShouldMatchers
-import org.scalatest.FlatSpec
-import scala.collection.JavaConversions._
-import scala.concurrent.ops._
 
+import scala.collection.JavaConversions._
+
+import org.scalatest.{BeforeAndAfterAll, FunSuite}
+import org.scalatest.matchers.ShouldMatchers
+
+
+/**
+ * Test for the Shark server.
+ */
 class SharkServerSuite extends FunSuite with BeforeAndAfterAll with ShouldMatchers
   with CliTestToolkit {
 
@@ -24,36 +24,14 @@ class SharkServerSuite extends FunSuite with BeforeAndAfterAll with ShouldMatche
 
   Class.forName(DRIVER_NAME)
 
-  override def beforeAll() {
-    launchServer()
-    createTable
-    createCachedTable
-    val stmt = createStatement
-    val baseFilePath = System.getenv("HIVE_DEV_HOME")
-    stmt.executeQuery("create table clicks(id int, click int) row format delimited fields terminated by '\t'")
-    stmt.executeQuery("load data local inpath '${env:HIVE_DEV_HOME}/data/files/clicks.txt' OVERWRITE INTO TABLE clicks")
-    stmt.executeQuery("create table users(id int, name string) row format delimited fields terminated by '\t'")
-    stmt.executeQuery("load data local inpath '${env:HIVE_DEV_HOME}/data/files/users.txt' OVERWRITE INTO TABLE users")
-    stmt.executeQuery("create table clicks_cached as select * from clicks")
-    stmt.executeQuery("create table users_cached as select * from users")
-    stmt.executeQuery("create table test_bigint (key bigint, val string)")
-    stmt.executeQuery("load data local inpath '${env:HIVE_DEV_HOME}/data/files/kv1.txt' OVERWRITE INTO TABLE test_bigint")
-  }
+  override def beforeAll() { launchServer() }
 
-  override def afterAll() {
-    dropServerTable()
-    dropServerTable("foo_cached")
-    dropServerTable("clicks")
-    dropServerTable("users")
-    dropServerTable("test_bigint")
-    dropServerTable("test_bigint_cached")
-    dropServerTable("a")
-    dropServerTable("a_cached")
-    dropCachedTable
-    stopServer()
-  }
+  override def afterAll() { stopServer() }
 
   private def launchServer(args: Seq[String] = Seq.empty) {
+    // Forking a new process to start the Shark server. The reason to do this is it is
+    // hard to clean up Hive resources entirely, so we just start a new process and kill
+    // that process for cleanup.
     val defaultArgs = Seq("./bin/shark", "--service", "sharkserver",
       "-hiveconf",
       "javax.jdo.option.ConnectionURL=jdbc:derby:;databaseName=" + METASTORE_PATH + ";create=true",
@@ -71,163 +49,30 @@ class SharkServerSuite extends FunSuite with BeforeAndAfterAll with ShouldMatche
     process.waitFor()
   }
 
+  test("test query execution against a shark server") {
 
-  test("Count Distinct ") {
-    val stmt = createStatement
-    val rs = stmt.executeQuery("select count(distinct key) from test")
-    rs.next;
-    stmt.close
-    val count = rs.getInt(1)
-    count should equal(309)
-  }
-
-  test("Count bigint ") {
-    val stmt = createStatement
-    stmt.executeQuery("""create table test_bigint_cached as select * from test_bigint""")
-    val rs = stmt.executeQuery("select val, count(*) from test_bigint_cached where key=484 group by val")
-    rs.next;
-    stmt.close
-    val count = rs.getInt(2)
-    count should equal(1)
-  }
-
-  test("Read from existing table") {
-    val stmt = createStatement
-    val rs = stmt.executeQuery("select   count(*) from test")
-    rs.next ;
-    stmt.close
-    val count = rs.getInt(1)
-    count should equal (500)
-  }
-
-  test("column pruning, join condition1") {
-    val stmt = createStatement
-    val rs = stmt.executeQuery("select count(*) from test_cached left outer join test on (test_cached.key = test.key) ")
-    //check if this worked
-    rs.next ;
-    stmt.close
-    val count = rs.getInt(1)
-    count should equal (1028)
-  }
-
-  test("column pruning filters") {
-    val stmt = createStatement
-    val rs = stmt.executeQuery("select count(*) from test_cached where key > -1 ")
-    //check if this worked
-    rs.next ;
-    stmt.close
-    val count = rs.getInt(1)
-    count should equal (500)
-  }
-
-  test("column pruning group by") {
-    val stmt = createStatement
-    val rs = stmt.executeQuery("select key, count(*) from test_cached group by key ")
-    //check if this worked
-    rs.next ;
-    stmt.close
-    val count = rs.getInt(1)
-    count should equal (487)
-  }
-
-  test("column pruning group by with single filter") {
-    val stmt = createStatement
-    val rs = stmt.executeQuery("select key, count(*) from test_cached where val='val_484' group by key ")
-    //check if this worked
-    rs.next ;
-    stmt.close
-    val count = rs.getInt(1)
-    count should equal (484)
-  }
-
-  test("column pruning aggregate function") {
-    val stmt = createStatement
-    val rs = stmt.executeQuery("select val, sum(key) from test_cached group by val order by val desc")
-    //check if this worked
-    rs.next;
-    stmt.close
-    val count = rs.getInt(2)
-    count should equal(196)
-  }
-
-  test("insert into cached table") {
-    val stmt = createStatement
-    stmt.executeQuery("insert into table test_cached select * from test where key > -1")
-    val rs = stmt.executeQuery("select count(*) from test_cached")
-    //check if this worked
-    rs.next;
-    val count = rs.getInt(1)
-    stmt.close
-    count should equal(1000)
-  }
-
-  test("mapside join") {
-    val stmt = createStatement
-    val rs = stmt.executeQuery("select users.name, count(clicks.click) from clicks join users on (clicks.id = users.id) group by users.name having users.name='A'")
-    //check if this worked
-    rs.next;
-    stmt.close
-    val value = rs.getString(1)
-    value should equal("A")
-    val count = rs.getInt(2)
-    count should equal(3)
-  }
-
-  test("mapside join2") {
-    val stmt = createStatement
-    val rs = stmt.executeQuery("select count(*) from clicks join users on (clicks.id = users.id) ")
-    //check if this worked
-    rs.next;
-    stmt.close
-    val value = rs.getInt(1)
-    value should equal(5)
-  }
-
-  test("drop partition") {
-    val stmt = createStatement
-    stmt.executeQuery("""create table foo_cached(key int, val string) partitioned by (dt string)""")
-    stmt.executeQuery("insert overwrite table foo_cached partition(dt='100') select * from test")
-    //at this point we should have 500 entries
-    val rs = stmt.executeQuery("select count(*) from foo_cached")
-    //check if this worked
-    rs.next;
-    val count = rs.getInt(1)
-    stmt.close
-    count should equal(500)
-    val stmt2 = createStatement
-    stmt2.executeQuery("alter table foo_cached drop partition(dt='100')")
-    val rs2 = stmt2.executeQuery("select count(*) from foo_cached")
-    rs2.next;
-    val count2 = rs2.getInt(1)
-    stmt2.close
-    count2 should equal(0)
-  }
-
-  def getConnection:Connection  = DriverManager.getConnection("jdbc:hive://localhost:10000/default", "", "")
-
-  def createStatement:Statement = {
-    getConnection.createStatement()
-  }
-
-  def createTable(implicit table:String = TABLE) = {
-    val dataFilePath = System.getenv("HIVE_DEV_HOME") + "/data/files/kv1.txt"
-    val stmt = createStatement
+    val dataFilePath = CliTestToolkit.dataFilePath + "/kv1.txt"
+    val stmt = createStatement()
     stmt.executeQuery("DROP TABLE IF EXISTS test")
+    stmt.executeQuery("DROP TABLE IF EXISTS test_cached")
     stmt.executeQuery("CREATE TABLE test(key int, val string)")
     stmt.executeQuery("LOAD DATA LOCAL INPATH '" + dataFilePath+ "' OVERWRITE INTO TABLE test")
+    stmt.executeQuery("CREATE TABLE test_cached as select * from test limit 499")
+
+    var rs = stmt.executeQuery("select count(*) from test")
+    rs.next()
+    rs.getInt(1) should equal (500)
+
+    rs = stmt.executeQuery("select count(*) from test_cached")
+    rs.next()
+    rs.getInt(1) should equal (499)
+
+    stmt.close()
   }
 
-  def createCachedTable() = {
-    val stmt = createStatement
-    stmt.executeQuery("DROP TABLE IF EXISTS test_cached")
-    stmt.executeQuery("CREATE TABLE test_cached as select * from test")
+  def getConnection(): Connection = {
+    DriverManager.getConnection("jdbc:hive://localhost:10000/default", "", "")
   }
 
-  def dropServerTable(table: String = TABLE) = {
-    val stmt = createStatement
-    val sql = "DROP TABLE " + table
-    val rs = stmt.executeQuery(sql)
-  }
-
-  def dropCachedTable = dropServerTable(TABLE + "_cached")
+  def createStatement(): Statement = getConnection().createStatement()
 }
