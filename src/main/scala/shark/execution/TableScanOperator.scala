e2d1cb310d483b00b929e8c8c8fd7acee4d4879a
Mark Hamstra <markhamstra@gmail.com>
2013-07-14 17:59:45 -0700

    Changed error message wording

--- src/main/scala/shark/execution/TableScanOperator.scala
+++ src/main/scala/shark/execution/TableScanOperator.scala
@@ -111,7 +111,7 @@ class TableScanOperator extends TopOperator[HiveTableScanOperator] with HiveTopO
     if (cacheMode == CacheType.heap) {
       // Table should be in Spark heap (block manager).
       val rdd = SharkEnv.memoryMetadataManager.get(tableKey).getOrElse {
-        logError("""|Cached table %s not found in block manager.
+        logError("""|Table %s not found in block manager.
                     |Are you trying to access a cached table from a Shark session other than
                     |the one in which it was created?""".stripMargin.format(tableKey))
         throw(new QueryExecutionException("Cached table not found"))
