dec2a1f76d4af58faf356556a891afeeb2c631e9
Andy Konwinski <andyk@berkeley.edu>
2013-08-02 12:15:46 -0700

    Download Hadoop, do things in a workspace dir.

--- jenkins-test.sh
+++ jenkins-test.sh
@@ -1,6 +1,22 @@
 #! /usr/bin/env sh
 set -x
 
+
+if [ "x$SPARK_PROJ_DIR" == "x" ] ; then
+  pushd `dirname $0` > /dev/null
+  SPARK_PROJ_DIR=`pwd`
+  popd > /dev/null
+fi
+
+# Since this script downloads a lot of stuff, keep it in a subdir workspace.
+WORKSPACE="$SPARK_PROJ_DIR/jenkins-test-workspace"
+if [ "x$WORKSPACE" == "x/" ] ; then
+  exit '$WORKSPACE cannot be / since we delete and recreate it.'
+fi
+rm -rf $WORKSPACE
+mkdir $WORKSPACE
+pushd $WORKSPACE
+
 export SCALA_VERSION=2.9.3
 
 if [ "x$JAVA_HOME" == "x" ] ; then
@@ -11,10 +27,12 @@ if [ "x$SCALA_HOME" == "x" ] ; then
   exit "ERROR: You must set SCALA_HOME env var."
 fi
 
-if [ "x$WORKSPACE" == "x" ] ; then
-  pushd `dirname $0` > /dev/null
-  WORKSPACE=`pwd`
-  popd > /dev/null
+if [ "x$HADOOP_VERSION" == "x" ] ; then
+  HADOOP_VERSION="0.20.205.0"
+fi
+
+if [ "x$HADOOP_MAJOR_VERSION" == "x" ] ; then
+  HADOOP_MAJOR_VERSION=1
 fi
 
 # Clean up past Spark artifacts published locally.
@@ -24,12 +42,18 @@ rm -rf ~/.ivy2/cache/org.spark*
 # Download and build Spark.
 git clone https://github.com/mesos/spark.git
 pushd spark
+# Replace Hadoop1 settings in build file, which are the default, with Hadoop2 settings.
+sed -i.backup "s/val HADOOP_VERSION = \"1\.0\.4\"/val HADOOP_VERSION = \"$HADOOP_VERSION\"/" project/SparkBuild.scala
+sed -i.backup "s/val HADOOP_MAJOR_VERSION = \"1\"/val HADOOP_MAJOR_VERSION = \"$HADOOP_MAJOR_VERSION\"/" project/SparkBuild.scala
+# Build spark and push the jars to local Ivy/Maven caches.
 sbt/sbt clean publish-local
 popd
 export SPARK_HOME="$WORKSPACE/spark"
 
-# Do we need to checkout and setup an HDFS?
-#export HADOOP_HOME="$WORKSPACE/hadoop-0.20.205.0"
+# Download and unpack Hadoop and set env variable.
+wget http://archive.apache.org/dist/hadoop/core/hadoop-${HADOOP_VERSION}/hadoop-${HADOOP_VERSION}.tar.gz
+tar xvfz hadoop-${HADOOP_VERSION}.tar.gz
+export HADOOP_HOME="$WORKSPACE/hadoop-${HADOOP_VERSION}"
 
 # Instead of making and editing a copy of conf/shark-env.sh.template
 # just set env variables Shark needs here.
@@ -68,5 +92,7 @@ done
 popd
 
 # Compile and run the Shark tests.
-sbt/sbt test:compile
+sbt/sbt test
+
+# Hive CLI Tests
 bin/dev/test
