f93216eef2dbd18a2e63c5687d39e02f0cff61f8
Harvey Feng <harvey@databricks.com>
2013-09-23 11:57:08 -0700

    Skeleton for SparkDDLTask.
    A dependent/child task of Hive's DDLTask that executes Shark-specific metadata handlers.

---
+++ src/main/scala/shark/execution/SparkDDLTask.scala
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2012 The Regents of The University California.
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package shark.execution
+
+import org.apache.hadoop.hive.ql.{Context, DriverContext}
+import org.apache.hadoop.hive.ql.exec.{Task => HiveTask, TaskExecutionException}
+import org.apache.hadoop.hive.ql.metadata.Hive
+import org.apache.hadoop.hive.ql.plan._
+import org.apache.hadoop.hive.ql.plan.api.StageType
+
+import org.apache.spark.SparkEnv
+
+import shark.{LogHelper, SharkEnv}
+
+
+class SparkDDLWork(val ddlDesc: DDLDesc) extends java.io.Serializable
+
+class SparkDDLTask extends HiveTask[SparkDDLWork] with Serializable with LogHelper {
+
+  override def execute(driverContext: DriverContext): Int = {
+    val sparkEnv = SparkEnv.get
+    val hiveMetadataDb = Hive.get(conf)
+
+    work.ddlDesc match {
+      case creatTblDesc: CreateTableDesc => {
+        createTable(sparkEnv, hiveMetadataDb, creatTblDesc)
+      }
+      case addPartitionDesc: AddPartitionDesc => {
+        addPartition(sparkEnv, hiveMetadataDb, addPartitionDesc)
+      }
+      case renamePartitionDesc: RenamePartitionDesc => {
+        renamePartition(sparkEnv, hiveMetadataDb, renamePartitionDesc)
+      }
+      case dropTableDesc: DropTableDesc => {
+        dropTable(sparkEnv, hiveMetadataDb, dropTableDesc)
+      }
+      case _ => {
+        throw new UnsupportedOperationException(
+          "Shark does not require specific DDL handling for: " + work.ddlDesc.getClass.getName)
+      }
+    }
+
+    // Hive's task runner expects a '0' return value to indicate success and exceptions on
+    // failure.
+    return 0
+  }
+
+  def createTable(sparkEnv: SparkEnv, hiveMetadataDb: Hive, createTblDesc: CreateTableDesc): Int = {
+    return 0
+  }
+
+  def addPartition(
+      sparkEnv: SparkEnv,
+      hiveMetadataDb: Hive,
+      addPartitionDesc: AddPartitionDesc): Int = {
+    return 0
+  }
+
+  def renamePartition(
+      sparkEnv: SparkEnv,
+      hiveMetadataDb: Hive,
+      renamePartitionDesc: RenamePartitionDesc): Int = {
+    return 0
+  }
+
+  def dropTable(sparkEnv: SparkEnv, hiveMetadataDb: Hive, dropTableDesc: DropTableDesc): Int = {
+    return 0
+  }
+
+  override def getType = StageType.DDL
+
+  override def getName = "DDL-SPARK"
+
+  override def localizeMRTmpFilesImpl(ctx: Context) = Unit
+}
